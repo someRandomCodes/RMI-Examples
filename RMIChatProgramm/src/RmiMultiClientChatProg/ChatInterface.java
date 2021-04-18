33e0d319f404a26dd554f2d91287b8da5ad009d2
Thomas G.S <somerandomcodes@gmail.com>
2021-04-18 15:14:46 +0200

    initial commit

---
+++ RMIChatProgramm/src/RmiMultiClientChatProg/ChatInterface.java
@@ -0,0 +1,9 @@
+package RmiMultiClientChatProg;
+
+import java.rmi.RemoteException;
+
+public interface ChatInterface extends java.rmi.Remote {
+    boolean checkClientCredintials(ChatInterface ci,String name, String pass) throws RemoteException;
+    void broadcastMessage(String name,String message) throws RemoteException;
+    void sendMessageToClient(String message) throws RemoteException;
+}
